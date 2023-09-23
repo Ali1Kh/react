0f27fc3ff96563219928b0af2978546e8dba7688
Ali Khaled <alielsaadany94@gmail.com>
2023-09-23 19:29:46 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Start from "./components/start/Start";
-const router = createBrowserRouter([
+const router = createHashRouter([
   {path:"start",element: <Start/> ,children:[
   { path: "", element: <Home /> },
   { path: "home", element: <Home /> },
